4cc1be1311c846522b1377dd9205f8cfb0239cef
Tom Green <tom@chaoscube.xyz>
2016-07-22 21:52:29 +0100

    Initial hello world

---
+++ src/main/com/chaoscube/algos/algos.java
@@ -0,0 +1,19 @@
+package main.com.chaoscube.algos;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by tom on 21/07/16.
+ */
+public class algos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Algos");
+
+        Logger logger = LoggerFactory.getLogger("algosLogger");
+
+        logger.info("Hello algos logger");
+    }
+}
